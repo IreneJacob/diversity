c4f3da870e693630e423aed72b61ad9a8cd81714
IreneJacob <jacobi@usi.ch>
2019-02-27 00:53:38 +0100

    transaction_db_playground created
    further tests required

--- rocksdb_playground/include/functions.h
+++ rocksdb_playground/include/functions.h
@@ -1,17 +1,29 @@
 #include <rocksdb/c.h>
 
-
-struct rocksdb_params {
-  rocksdb_t *db;
-  rocksdb_options_t *options;
-  rocksdb_writeoptions_t *writeoptions;
-  rocksdb_readoptions_t *readoptions;
-  rocksdb_checkpoint_t *cp;
+struct rocksdb_txdb_params {
+    rocksdb_transactiondb_t *tx_db;
+    rocksdb_options_t *options;
+    rocksdb_transactiondb_options_t *txdb_options;
+    rocksdb_writeoptions_t *write_options;
+    rocksdb_readoptions_t *read_options;
+    rocksdb_transaction_t *tx;
+    rocksdb_transaction_options_t* tx_options;
 };
 
-void init_rocksdb(void);
-int deconstruct(void);
-int parse_lineinput(char *lineinput, char* *array, int *count);
-int handle_requests(char** array, int count);
+void init_rocksdb_txdb(void);
+int txdb_deconstruct(void);
+int open_txdb(char* db_path);
+int close_txdb(void);
+int destroy_txdb(char* db_path);
+int begin_tx(void);
+int commit_tx(void);
+int delete_tx(void);
+int handle_tx_read(char *key, size_t keylen);
+int handle_tx_write(char *key, size_t keylen, char *val, size_t vallen);
+int handle_tx_delete(char *key, size_t keylen);
+int handle_tx_list(void);
+int handle_txdb_list(void);
+int handle_save(void);
+int handle_rollback(void);
 
-extern struct rocksdb_params rocks;
+extern struct rocksdb_txdb_params txrocks;
--- rocksdb_playground/src/datastore.c
+++ rocksdb_playground/src/datastore.c
@@ -6,162 +6,252 @@
 
 #include "functions.h"
 
-void init_rocksdb(void)
-{
+void init_rocksdb_txdb(void){
   uint64_t mem_budget = 1048576;
-  rocks.options = rocksdb_options_create();
+  txrocks.options = rocksdb_options_create();
   long cpus = sysconf(_SC_NPROCESSORS_ONLN);
-  rocksdb_options_increase_parallelism(rocks.options, (int)(cpus));
-  rocksdb_options_optimize_level_style_compaction(rocks.options, mem_budget);
-  rocksdb_options_set_create_if_missing(rocks.options, 1);
-  rocks.writeoptions = rocksdb_writeoptions_create();
-  rocksdb_writeoptions_disable_WAL(rocks.writeoptions, 1);
-  rocksdb_writeoptions_set_sync(rocks.writeoptions, 0);
-  rocks.readoptions = rocksdb_readoptions_create();
+  rocksdb_options_increase_parallelism(txrocks.options, (int)(cpus));
+  rocksdb_options_optimize_level_style_compaction(txrocks.options, mem_budget);
+  rocksdb_options_set_create_if_missing(txrocks.options, 1);
+
+  txrocks.txdb_options = rocksdb_transactiondb_options_create();
+  txrocks.tx_options = rocksdb_transaction_options_create();
+  txrocks.write_options = rocksdb_writeoptions_create();
+  rocksdb_writeoptions_disable_WAL(txrocks.write_options, 1);
+  rocksdb_writeoptions_set_sync(txrocks.write_options, 0);
+  txrocks.read_options = rocksdb_readoptions_create();
 }
 
-inline int create_checkpoint_object() {
-  char *err = NULL;
-  rocks.cp = rocksdb_checkpoint_object_create(rocks.db, &err);
+int txdb_deconstruct(void){
+  if (txrocks.txdb_options) {
+    rocksdb_transactiondb_options_destroy(txrocks.txdb_options);
+  }
+  if (txrocks.tx) {
+    rocksdb_transaction_destroy(txrocks.tx);
+  }
+  if (txrocks.tx_db)
+      rocksdb_transactiondb_close(txrocks.tx_db);
+  return 0;
+}
+
+inline int txdb_is_open(void) {
+    if (txrocks.tx_db == NULL) {
+        fprintf(stderr, "DB is not opened\n");
+        return 0;
+    }
+    return 1;
+}
+
+int open_txdb(char* db_path){
+  char* err = NULL;
+  txrocks.tx_db = rocksdb_transactiondb_open( txrocks.options, txrocks.txdb_options, db_path, &err);
   if (err != NULL) {
-    printf("Cannot create checkpoint object: %s\n", err);
-    return -2;
+      printf("Cannot open DB: %s\n", err);
+      return -1;
   }
   return 0;
 }
 
-inline int is_open(void) {
-  if (rocks.db == NULL) {
-    fprintf(stderr, "DB is not openned\n");
-    return 0;
+int close_txdb(void){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return 0;
   }
-  return 1;
+  rocksdb_transactiondb_close(txrocks.tx_db);
+  txrocks.tx_db = NULL;
+  return 0;
 }
 
-int open_db(char* db_path) {
+int destroy_txdb(char* db_path){
   char* err = NULL;
-  rocks.db = rocksdb_open(rocks.options, db_path, &err);
+  rocksdb_destroy_db(txrocks.options, db_path, &err);
   if (err != NULL) {
-    printf("Cannot open DB: %s\n", err);
+      printf("Cannot destroy DB: %s\n", err);
+      return -1;
+  }
+  return 0;
+}
+
+int begin_tx(void){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  txrocks.tx = rocksdb_transaction_begin(txrocks.tx_db, txrocks.write_options, txrocks.tx_options, NULL);
+  return 0;
+}
+int commit_tx(void){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  char * err = NULL;
+  rocksdb_transaction_commit(txrocks.tx, &err);
+  if (err != NULL) {
+    printf("%s\n", "Error committing transaction" );
+    return -1;
+  }
+  return 0;
+}
+int delete_tx(void){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  if (txrocks.tx) {
+    rocksdb_transaction_destroy(txrocks.tx);
+  }
+  return 0;
+}
+int handle_tx_read(char *key, size_t keylen){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  char *err = NULL;
+  char *val;
+  size_t vallen;
+
+  val = rocksdb_transaction_get(txrocks.tx, txrocks.read_options, key, keylen, &vallen, &err);
+  if (err != NULL) {
+      fprintf(stdout, "Read Error: %s\n", err);
+      return -1;
+  }
+  if (val == NULL) {
+      printf("Value was not found for key %s\n", key);
+      return 0;
+  }
+  printf("Found value %s, vallen %lu for key %s\n", val, vallen, key);
+  free(val);
+  return 0;
+}
+
+int handle_tx_write(char *key, size_t keylen, char *val, size_t vallen){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  char * err = NULL;
+  rocksdb_transaction_put(txrocks.tx, key, keylen, val, vallen, &err);
+  if (err != NULL) {
+    printf("%s %s, %s\n", "Error writing key value pair", key, val);
+    return -1;
+  }
+  return 0;
+}
+int handle_tx_delete(char *key, size_t keylen){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  char * err = NULL;
+  rocksdb_transaction_delete(txrocks.tx, key , keylen, &err);
+  if (err != NULL) {
+    printf("%s %s\n", "Error deleting entry", key);
     return -1;
   }
   return 0;
 }
 
-int close_db(void) {
-  if (!is_open()) {
-    return 0;
+int handle_save(){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
   }
-  rocksdb_close(rocks.db);
+  if (txrocks.tx == NULL) {
+    fprintf(stderr, "No transaction created\n");
+      return -1;
+  }
+  rocksdb_transaction_set_savepoint(txrocks.tx);
   return 0;
 }
 
-int deconstruct(void) {
-  rocksdb_checkpoint_object_destroy(rocks.cp);
-  rocksdb_close(rocks.db);
+int handle_rollback(){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  if (txrocks.tx == NULL) {
+    fprintf(stderr, "No transaction created\n");
+      return -1;
+  }
+  char * err = NULL;
+  rocksdb_transaction_rollback_to_savepoint(txrocks.tx, &err);
+  if (err != NULL) {
+    printf("%s\n", "Error in rolling back to savepoint");
+    return -1;
+  }
   return 0;
 }
 
-void handle_read(char *key, size_t keylen)
-{
-  if (!is_open()) {
-    return;
-  }
-  char *err = NULL;
-  char *val;
-  size_t vallen;
-  val = rocksdb_get(rocks.db, rocks.readoptions, key, keylen, &vallen, &err);
-  if (err != NULL) {
-    fprintf(stdout, "Read Error: %s\n", err);
-    return;
-  }
-  if (val == NULL) {
-    printf("Value was not found for key %s\n", key);
-    return;
-  }
-  printf("Found value %s, vallen %zu for key %s\n", val, vallen, key);
-  free(val);
-}
 
-void handle_delete(char *key, size_t keylen)
-{
-  if (!is_open()) {
-    return;
-  }
-  char *err = NULL;
-  rocksdb_delete(rocks.db, rocks.writeoptions, key, keylen, &err);
-  if (err != NULL) {
-    fprintf(stdout, "Delete Error: %s\n", err);
-    return;
-  }
-}
-
-void handle_write(char *key, size_t keylen, char *val, size_t vallen)
-{
-  if (!is_open()) {
-    return;
-  }
-  char *err = NULL;
-  rocksdb_put(rocks.db, rocks.writeoptions, key, keylen, val, vallen, &err);
-  if (err != NULL) {
-    fprintf(stdout, "Write Error: %s\n", err);
-  }
-}
-
-void handle_checkpoint(char* checkpoint_dir) {
-  if (!is_open()) {
-    return;
-  }
-  char *err = NULL;
-  uint64_t log_size_for_flush = 8192;
-  if (rocks.cp == NULL) {
-    create_checkpoint_object();
-  }
-  rocksdb_checkpoint_create(rocks.cp, checkpoint_dir, log_size_for_flush, &err);
-  if (err != NULL) {
-    fprintf(stdout, "Checkpoint Error: %s\n", err);
-  }
-}
-
-int handle_requests(char** argv, int count) {
-  if (strcmp("read", argv[0]) == 0) {
-    if (count < 2) {
-      fprintf(stderr, "Not enough argument for reading\n");
+int handle_tx_list(void){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
       return -1;
-    }
-    handle_read(argv[1], strlen(argv[1]));
   }
-  else if (strcmp("delete", argv[0]) == 0) {
-    if (count < 2) {
-      fprintf(stderr, "Not enough argument for deleting\n");
+  if (txrocks.tx == NULL) {
+    fprintf(stderr, "no such transaction\n");
       return -1;
-    }
-    handle_delete(argv[1], strlen(argv[1]));
   }
-  else if (strcmp("write", argv[0]) == 0) {
-    if (count < 3) {
-      fprintf(stderr, "Not enough argument for writing\n");
-      return -2;
-    }
-    handle_write(argv[1], strlen(argv[1]), argv[2], strlen(argv[2]));
-  }
-  else if (strcmp("open", argv[0]) == 0) {
-    if (count < 2) {
-      fprintf(stderr, "Missing arguments for opening database\n");
-      return -3;
-    }
-    return open_db(argv[1]);
-  }
-  else if (strcmp("close", argv[0]) == 0) {
-    return close_db();
-  }
-  else if (strcmp("checkpoint", argv[0]) == 0) {
-    if (count < 2) {
-      fprintf(stderr, "Not enough argument for checkpointing\n");
+  rocksdb_iterator_t* iterator = rocksdb_transaction_create_iterator(txrocks.tx, txrocks.read_options);
+  if (!iterator) {
+      fprintf(stderr, "Create Iterator has an error\n");
       return -1;
-    }
-    handle_checkpoint(argv[1]);
+  }
+  printf("%-20s | %-20s\n", "Key", "Value");
+  printf("------------------------------------------------\n");
+  for (rocksdb_iter_seek_to_first(iterator);
+          rocksdb_iter_valid(iterator);
+          rocksdb_iter_next(iterator))
+  {
+      size_t klen;
+      const char *key = rocksdb_iter_key(iterator, &klen);
+      size_t vlen;
+      const char *value = rocksdb_iter_value(iterator, &vlen);
+
+      char ikey[klen+1];
+      strncpy(ikey, key, klen);
+      ikey[klen] = '\0';
+      char ivalue[vlen+1];
+      strncpy(ivalue, value, vlen);
+      ivalue[vlen] = '\0';
+      printf("%-20s | %-20s\n", ikey, ivalue);
+  }
+  rocksdb_iter_destroy(iterator);
+  return 0;
+}
+
+int handle_txdb_list(void){
+  if (txrocks.tx_db == NULL) {
+    fprintf(stderr, "DB is not opened\n");
+      return -1;
+  }
+  rocksdb_iterator_t* iterator = rocksdb_transactiondb_create_iterator(txrocks.tx_db, txrocks.read_options);
+  if (!iterator) {
+      fprintf(stderr, "Create Iterator has an error\n");
+      return -1;
   }
 
+  printf("%-20s | %-20s\n", "Key", "Value");
+  printf("------------------------------------------------\n");
+  for (rocksdb_iter_seek_to_first(iterator);
+          rocksdb_iter_valid(iterator);
+          rocksdb_iter_next(iterator))
+  {
+      size_t klen;
+      const char *key = rocksdb_iter_key(iterator, &klen);
+      size_t vlen;
+      const char *value = rocksdb_iter_value(iterator, &vlen);
+
+      char ikey[klen+1];
+      strncpy(ikey, key, klen);
+      ikey[klen] = '\0';
+      char ivalue[vlen+1];
+      strncpy(ivalue, value, vlen);
+      ivalue[vlen] = '\0';
+      printf("%-20s | %-20s\n", ikey, ivalue);
+  }
+  rocksdb_iter_destroy(iterator);
   return 0;
 }
